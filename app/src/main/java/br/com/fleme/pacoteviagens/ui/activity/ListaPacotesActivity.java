ecff4ac918d23e02146194eb496dcab703f59527
Felipe Augusto de Souza Leme <fleme@e-deploy.local>
2018-09-18 19:13:17 -0300

    Criando a primeira activity.

---
+++ app/src/main/java/br/com/fleme/pacoteviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,15 @@
+package br.com.fleme.pacoteviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.fleme.pacoteviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
