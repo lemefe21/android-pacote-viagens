8f85d02f8696a2f3d3ac6b31c4090b46d26f57e8
Felipe Augusto de Souza Leme <fleme@e-deploy.local>
2018-09-26 17:57:50 -0300

    Pegando as referências da view dentro do adapter e setando os valores a partir do nosso mock da classe DAO.

--- app/src/main/java/br/com/fleme/pacoteviagens/ui/adapter/ListaPacotesAdapter.java
+++ app/src/main/java/br/com/fleme/pacoteviagens/ui/adapter/ListaPacotesAdapter.java
@@ -1,10 +1,16 @@
 package br.com.fleme.pacoteviagens.ui.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -40,6 +46,23 @@ public class ListaPacotesAdapter extends BaseAdapter {
     public View getView(int posicao, View view, ViewGroup parent) {
 
         View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_pacote, parent, false);
+
+        Pacote pacote = pacotes.get(posicao);
+
+        TextView local = viewCriada.findViewById(R.id.item_pacote_local);
+        local.setText(pacote.getLocal());
+
+        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_imagem);
+        Resources resources = context.getResources();
+        int idDrawable = resources.getIdentifier(pacote.getImagem(), "drawable", context.getPackageName());
+        Drawable drawable = resources.getDrawable(idDrawable);
+        imagem.setImageDrawable(drawable);
+
+        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
+        dias.setText(pacote.getDias() + " dias");
+
+        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
+        preco.setText("R$ " + pacote.getPreco().toString());
 
         return viewCriada;
     }
