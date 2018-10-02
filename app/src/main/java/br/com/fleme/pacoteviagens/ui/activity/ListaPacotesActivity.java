9664c8e1e2f9550500cdf3b45f338e69f5f02cd2
Felipe Augusto de Souza Leme <fleme@e-deploy.local>
2018-10-02 18:32:34 -0300

    Adicionando o botão que irá realizar o pacote e implementando o bing com informações fixas para testar o layout pronto.

--- app/src/main/java/br/com/fleme/pacoteviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/br/com/fleme/pacoteviagens/ui/activity/ListaPacotesActivity.java
@@ -1,5 +1,6 @@
 package br.com.fleme.pacoteviagens.ui.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,6 +34,9 @@ public class ListaPacotesActivity extends AppCompatActivity {
 
         //Alt+Shift+F10 >> Run
         //Shift+F10 >> Run
+
+        Intent intent = new Intent(this, ResumoPacoteActivity.class);
+        startActivity(intent);
 
     }
 
--- app/src/main/java/br/com/fleme/pacoteviagens/ui/activity/ResumoPacoteActivity.java
+++ app/src/main/java/br/com/fleme/pacoteviagens/ui/activity/ResumoPacoteActivity.java
@@ -1,9 +1,21 @@
 package br.com.fleme.pacoteviagens.ui.activity;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.math.BigDecimal;
 
 import br.com.fleme.pacoteviagens.R;
+import br.com.fleme.pacoteviagens.model.Pacote;
+import br.com.fleme.pacoteviagens.util.DiasUtil;
+import br.com.fleme.pacoteviagens.util.MoedaUtil;
+import br.com.fleme.pacoteviagens.util.ResourceUtil;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
@@ -11,5 +23,25 @@ public class ResumoPacoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        //processo de bind
+
+        ImageView imagem = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawable = ResourceUtil.retornaDrawable(this, pacoteSaoPaulo.getImagem());
+        imagem.setImageDrawable(drawable);
+
+        TextView local = findViewById(R.id.resumo_pacote_local);
+        local.setText(pacoteSaoPaulo.getLocal());
+
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
+        String diasEmTexto = DiasUtil.formataEmTexto(pacoteSaoPaulo.getDias());
+        dias.setText(diasEmTexto);
+
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
+        String moedaBrasileira = MoedaUtil.formataMoedaParaPtBr(pacoteSaoPaulo.getPreco());
+        preco.setText(moedaBrasileira);
+
     }
 }
