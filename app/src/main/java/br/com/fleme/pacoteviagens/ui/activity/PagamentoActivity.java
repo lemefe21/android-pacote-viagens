2c63a1ccf58996a71109070539cb60bf0e8cca4e
felleme <felleme@prestadores.amil.com.br>
2018-10-24 19:02:11 -0300

    Criando o layout da tela do resumo da compra.

--- app/src/main/java/br/com/fleme/pacoteviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/br/com/fleme/pacoteviagens/ui/activity/PagamentoActivity.java
@@ -1,11 +1,9 @@
 package br.com.fleme.pacoteviagens.ui.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 import java.math.BigDecimal;
 
 import br.com.fleme.pacoteviagens.R;
@@ -27,6 +25,9 @@ public class PagamentoActivity extends AppCompatActivity {
         Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
 
         mostraPreco(pacoteSaoPaulo);
+
+        Intent intent = new Intent(this, ResumoCompraActivity.class);
+        startActivity(intent);
 
     }
 
---
+++ app/src/main/java/br/com/fleme/pacoteviagens/ui/activity/ResumoCompraActivity.java
@@ -0,0 +1,15 @@
+package br.com.fleme.pacoteviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.fleme.pacoteviagens.R;
+
+public class ResumoCompraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_compra);
+    }
+}
