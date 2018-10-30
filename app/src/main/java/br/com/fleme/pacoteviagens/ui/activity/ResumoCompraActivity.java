aa089c487dd38b09713cbf857f41059f1604a79d
felleme <felleme@prestadores.amil.com.br>
2018-10-30 18:11:27 -0300

    Refatorando a activity e o layout do resumo da compra.

--- app/src/main/java/br/com/fleme/pacoteviagens/ui/activity/ResumoCompraActivity.java
+++ app/src/main/java/br/com/fleme/pacoteviagens/ui/activity/ResumoCompraActivity.java
@@ -1,15 +1,63 @@
 package br.com.fleme.pacoteviagens.ui.activity;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import br.com.fleme.pacoteviagens.R;
+import br.com.fleme.pacoteviagens.model.Pacote;
+import br.com.fleme.pacoteviagens.util.DataUtil;
+import br.com.fleme.pacoteviagens.util.MoedaUtil;
+import br.com.fleme.pacoteviagens.util.ResourceUtil;
 
 public class ResumoCompraActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Resumo da compra";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_compra);
+
+        setTitle(TITULO_APPBAR);
+
+        //utilizado para exemplo
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        mostraLocal(pacoteSaoPaulo);
+
+        mostraImagem(pacoteSaoPaulo);
+
+        mostraPeriodo(pacoteSaoPaulo);
+
+        mostraPreco(pacoteSaoPaulo);
+
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView precoPacote = findViewById(R.id.resumo_compra_preco_pacote);
+        String moedaBrasileira = MoedaUtil.formataMoedaParaPtBr(pacote.getPreco());
+        precoPacote.setText(moedaBrasileira);
+    }
+
+    private void mostraPeriodo(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_compra_data_viagem);
+        String periodoEmTexto = DataUtil.periodoEmTexto(pacote.getDias());
+        data.setText(periodoEmTexto);
+    }
+
+    private void mostraImagem(Pacote pacote) {
+        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
+        Drawable drawableDoPacote = ResourceUtil.retornaDrawable(this, pacote.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+    }
+
+    private void mostraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_compra_local_pacote);
+        local.setText(pacote.getLocal());
     }
 }
